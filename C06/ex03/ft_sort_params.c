d70ee8146cc136061b63de5c9ecc198449e346fe
ahaarij <ahmedaarij05@gmail.com>
2023-08-12 02:13:37 +0400

    C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/22 09:42:58 by ahaarij           #+#    #+#             */
+/*   Updated: 2023/08/11 19:47:22 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	print_arguments(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		i++;
+		ft_putchar('\n');
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*arg;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i;
+		while (ft_strcmp(argv[j], argv[j - 1]) < 0 && j > 1)
+		{
+			arg = argv[j - 1];
+			argv[j - 1] = argv[j];
+			argv[j] = arg;
+			j--;
+		}
+		i++;
+	}
+	print_arguments(argc, argv);
+	return (0);
+}
